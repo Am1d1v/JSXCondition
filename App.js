7877902bb0f205fb583df15f7c663e2e362b14c4
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-01 16:12:00 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -4,8 +4,9 @@ import PetInfo from './components/PetInfo';
 function App() {
   return (
     <div className="App">
-      <PetInfo animal="cat" age="10"/>
-      <PetInfo animal="dog" age="15"/>
+      <PetInfo animal="cat" age="10" hasPet = {true}/>
+      <PetInfo animal="dog" age="15" hasPet = {true}/>
+      <PetInfo animal="raven" age="6" hasPet = {false}/>
     </div>
   );
 }
